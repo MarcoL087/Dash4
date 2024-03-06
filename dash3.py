2955635b9a93be409891a05afd9bcac213bc0267
MarcoL087 <148775781+MarcoL087@users.noreply.github.com>
2024-03-06 21:22:44 +0100

    Update dash3.py

--- dash3.py
+++ dash3.py
@@ -32,6 +32,7 @@ initial_fig.update_layout(
 
 # Initializing app
 app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
 
 # App layout
 app.layout = html.Div(
